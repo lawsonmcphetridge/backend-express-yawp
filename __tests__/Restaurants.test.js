89ac9eb2f6cb5be874f782e17a7a9d5b1814382f
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-28 15:09:37 -0800

    added test for id

--- __tests__/Restaurants.test.js
+++ __tests__/Restaurants.test.js
@@ -1,26 +1,55 @@
-const app = require("../lib/app");
-const { request } = require("../lib/app");
-const pool = require("../lib/utils/pool");
-
+const setup = require('../data/setup');
+const app = require('../lib/app');
+const request = require('supertest');
+const pool = require('../lib/utils/pool');
 
 describe('rest routes', () => {
-    beforeEach(() => {
-        return setup(pool);
-    });
-    
-    
-    it('/api/v1/restaurants shows a list of restaurants', async () => {
-        const resp = await request(app).get('/api/v1/restaurants');
-        expect(resp.body).toMatchInlineSnapshot();
-    })
-    
-    
-    
-    
-    
-    
-    
-    afterAll(() => {
-        pool.end();
-    });
-})
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('/api/v1/restaurants shows a list of restaurants', async () => {
+    const resp = await request(app).get('/api/v1/restaurants');
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "cost": 1,
+          "cuisine": "American",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
+          "name": "Pip's Original",
+          "website": "http://www.PipsOriginal.com",
+        },
+        Object {
+          "cost": 3,
+          "cuisine": "Italian",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/13/af/df/89/duck.jpg",
+          "name": "Mucca Osteria",
+          "website": "http://www.muccaosteria.com",
+        },
+        Object {
+          "cost": 2,
+          "cuisine": "Mediterranean",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/1c/f2/e5/0c/dinner.jpg",
+          "name": "Mediterranean Exploration Company",
+          "website": "http://www.mediterraneanexplorationcompany.com/",
+        },
+        Object {
+          "cost": 2,
+          "cuisine": "American",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-o/0d/d6/a1/06/chocolate-gooey-brownie.jpg",
+          "name": "Salt & Straw",
+          "website": "https://saltandstraw.com/pages/nw-23",
+        },
+      ]
+    `);
+  });
+
+  it('/api/v1/restaurants/:id should return rest with nested review', async () => {
+    const resp = await request(app).get('/api/v1/restaurants');
+    expect(resp.body).toMatchInlineSnapshot();
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
