3db72c6f4690c2bbe6a67f4d945b4b7e2fb57cd3
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-28 15:55:33 -0800

    passed 2nd rest rest

--- __tests__/Restaurants.test.js
+++ __tests__/Restaurants.test.js
@@ -15,6 +15,7 @@ describe('rest routes', () => {
         Object {
           "cost": 1,
           "cuisine": "American",
+          "id": "1",
           "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
           "name": "Pip's Original",
           "website": "http://www.PipsOriginal.com",
@@ -22,6 +23,7 @@ describe('rest routes', () => {
         Object {
           "cost": 3,
           "cuisine": "Italian",
+          "id": "2",
           "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/13/af/df/89/duck.jpg",
           "name": "Mucca Osteria",
           "website": "http://www.muccaosteria.com",
@@ -29,6 +31,7 @@ describe('rest routes', () => {
         Object {
           "cost": 2,
           "cuisine": "Mediterranean",
+          "id": "3",
           "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/1c/f2/e5/0c/dinner.jpg",
           "name": "Mediterranean Exploration Company",
           "website": "http://www.mediterraneanexplorationcompany.com/",
@@ -36,6 +39,7 @@ describe('rest routes', () => {
         Object {
           "cost": 2,
           "cuisine": "American",
+          "id": "4",
           "image": "https://media-cdn.tripadvisor.com/media/photo-o/0d/d6/a1/06/chocolate-gooey-brownie.jpg",
           "name": "Salt & Straw",
           "website": "https://saltandstraw.com/pages/nw-23",
@@ -46,9 +50,46 @@ describe('rest routes', () => {
 
   it('/api/v1/restaurants/:id should return rest with nested review', async () => {
     const resp = await request(app).get('/api/v1/restaurants/1');
-    expect(resp.body).toMatchInlineSnapshot();
+    expect(resp.body).toMatchInlineSnapshot(`
+      Object {
+        "cost": 1,
+        "cuisine": "American",
+        "id": "1",
+        "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
+        "name": "Pip's Original",
+        "reviews": Array [
+          Object {
+            "detail": "Best restaurant ever!",
+            "id": "1",
+            "restaurant_id": "1",
+            "stars": 5,
+            "user_id": "1",
+          },
+          Object {
+            "detail": "Terrible service :(",
+            "id": "2",
+            "restaurant_id": "1",
+            "stars": 1,
+            "user_id": "2",
+          },
+          Object {
+            "detail": "It was fine.",
+            "id": "3",
+            "restaurant_id": "1",
+            "stars": 4,
+            "user_id": "3",
+          },
+        ],
+        "website": "http://www.PipsOriginal.com",
+      }
+    `);
   });
 
+    
+    
+    
+    
+    
   afterAll(() => {
     pool.end();
   });
