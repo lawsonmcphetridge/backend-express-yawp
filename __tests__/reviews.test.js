c5ea95ece5471888b7039d763cd871594df0ab7c
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-28 18:18:49 -0800

    passed delete test

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -1,9 +1,25 @@
-const { request } = require('../lib/app');
+const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
+describe('delete test', () => {
+  const registerAndLogin = async () => {
+    const agent = request.agent(app);
+    const user = await UserService.create(mockUser);
+    await agent
+      .post('/api/v1/users/sessions')
+      .send({ email: mockUser.email, password: mockUser.password });
+    return [agent, user];
+  };
 
-describe('delete test', async () => {
-  it('/api/v1/reviews should delete a revew', async () => {
-    const resp = await request(app).delete('/api/v1/reviews/1');
+  it('/api/v1/reviews/1 should delete a review', async () => {
+    const [agent] = await registerAndLogin();
+    const resp = await agent.delete('/api/v1/reviews/1');
     expect(resp.status).toBe(204);
 
     const getResp = await request(app).get('/api/v1/reviews/1');
